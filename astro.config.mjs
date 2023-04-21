1f506983f89d4b9013ef86724ffe56a69747e47b
Beimer Campos <beimercampos.123@gmail.com>
2023-04-20 21:46:57 -0500

    fixing domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site:'http://beimercampos.com/',
+  site:'https://dbeimer.github.io/',
   // base:,
   markdown: {
     drafts: true,
